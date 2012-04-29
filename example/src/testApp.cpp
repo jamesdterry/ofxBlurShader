dc5609671af2f28289bf1ab858140d78556b9190
James Hughes <james@virtualjames.com>
2012-04-29 16:39:02 -0700

    first commit

---
+++ example/src/testApp.cpp
@@ -0,0 +1,22 @@
+#include "testApp.h"
+
+void testApp::setup(){
+	camera.initGrabber(camWidth, camHeight);
+	blur.setup(camWidth, camHeight);
+}
+
+void testApp::update(){
+	camera.grabFrame();
+	ofSetWindowTitle(ofToString(ofGetFrameRate()));
+}
+
+void testApp::draw(){	
+	blur.setRadius(ofMap(mouseX, 0, ofGetWidth(), 0, 50, true));
+	blur.setPasses(ofMap(mouseY, 0, ofGetHeight(), 1, 50, true));
+	
+	blur.begin();
+	camera.draw(0, 0);
+	blur.end();
+	
+	ofDrawBitmapString(ofToString((int) ofGetFrameRate()), 10, 20);
+}
